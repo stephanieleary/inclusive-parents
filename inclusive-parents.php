8e5de5157b20cc58420a9b2a7044cf509af665e3
Stephanie Leary <steph@sillybean.net>
2016-09-23 11:01:25 -0500

    Rewrite, better labels, remove filter on public queries

--- inclusive-parents.php
+++ inclusive-parents.php
@@ -1,81 +1,104 @@
 <?php
 /*
 Plugin Name: Inclusive Parents
-Description: A tiny plugin to allow draft, private, scheduled, and password-protected pages to be selected as parents, displayed in menus, and included in page lists.
+Description: Allow draft, private, scheduled, and password-protected pages to be selected as parents and added to menus.
 Author: sillybean
-Version: 1.0.1
+Version: 1.1
 Author URI: http://stephanieleary.com
 License: GPL2
 Requires at least: 3.2
-Tested up to: 4.6
-Stable tag: 1.0.1
+Tested up to: 4.6.1
+Stable tag: 1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-
 /**
- * Enable unpublished pages in page lists and dropdowns
+ * Enable private and password-protected pages in page lists and dropdowns
  *
  * @param array $args
  * @return array $args
  */
 function scl_list_pages_args( $args ) {
-    $args['post_status'] = array( 'publish', 'private' );
+    $args['post_status'] = array( 'publish', 'private', 'password' );
     return $args;
 }
 
 add_filter( 'wp_page_menu_args', 'scl_list_pages_args' );
 add_filter( 'widget_pages_args', 'scl_list_pages_args' );
 
+
 /**
- * Add private/draft/future/pending pages to parent dropdown in page attributes metabox and Quick Edit
+ * Add private/draft/future/pending pages to parent dropdown in page attributes metabox, Quick Edit, and Bulk Edit
  *
  * @param array $dropdown_args
  * @param object $post (Optional)
  * @return array $dropdown_args
  */
 function scl_page_attributes_metabox_add_parents( $dropdown_args, $post = NULL ) {
-	$dropdown_args['post_status'] = array('publish', 'draft', 'pending', 'future', 'private');
+	$dropdown_args['post_status'] = array( 'publish', 'draft', 'pending', 'future', 'private' );
 	return $dropdown_args;
 }
 
 add_filter( 'page_attributes_dropdown_pages_args', 'scl_page_attributes_metabox_add_parents', 10, 2 ); 
-add_filter( 'quick_edit_dropdown_pages_args', 'scl_page_attributes_metabox_add_parents', 10);
+add_filter( 'quick_edit_dropdown_pages_args', 'scl_page_attributes_metabox_add_parents', 10 );
 
 /**
- * Add (status) to titles in page parent dropdowns
+ * Add (Status) to titles in page parent dropdowns
  *
  * @param string $title
  * @param object $page
  * @return string $title
  */
-function scl_page_parent_status_filter( $title, $page ) {
-	$status = $page->post_status;
-	if ($status !== __('publish'))
-		$title .= " ($status)";
+function scl_page_parent_dropdown_status_label( $title, $page ) {
+	if ( !is_admin() )
+		return $title;
+		
+	$post_status = $page->post_status;
+	if ( $post_status !== __( 'publish' ) ) {
+		$status = get_post_status_object( $post_status );
+		$title .= " ($status->label)";
+	}
 	return $title;
 }
 
-add_filter( 'list_pages', 'scl_page_parent_status_filter', 10, 2);
+add_filter( 'list_pages', 'scl_page_parent_dropdown_status_label', 10, 2 );
+
 
 /**
- * Filter public page queries to include privately published ones. 
- * Filter pages metabox on menu admin screen to include all built-in statuses.
+ * Add (Status) to titles in nav menu page checklists
+ *
+ * @param string $title
+ * @param object $page
+ * @return string $title
+ */
+function scl_menu_checklist_status_label( $title, $page_id ) {
+	if ( empty( $page_id ) )
+		return $title;
+		
+	if ( is_admin() && 'nav-menus' == get_current_screen()->base ) {	
+		$post_status = get_post_status( $page_id );
+		if ( $post_status !== __( 'publish' ) ) {
+			$status = get_post_status_object( $post_status );
+			$title .= " ($status->label)";
+		}
+	}
+	return $title;
+}
+
+add_filter( 'the_title', 'scl_menu_checklist_status_label', 10, 2 );
+
+/**
+ * Filter pages metabox on menu admin screen to include private pages.
  *
  * @param object $query
  * @return object $query
  */
-function scl_private_page_query_filter($query) {
-	if ( is_admin() ) {
-		$screen = get_current_screen();
-		if ( 'nav-menus' == $screen->base )
-			$query->set( 'post_status', 'publish,private,future,pending,draft' );
-	}
-	else {
-		$query->set( 'post_status', 'publish,private' );
+function scl_menu_screen_add_private_pages( $query ) {
+	if ( is_admin() && 'nav-menus' == get_current_screen()->base ) {
+		$query->set( 'post_status', array( 'publish', 'private', 'password' ) );
 	}	
 	return $query;
 }
 
-add_filter('pre_get_posts', 'scl_private_page_query_filter');
+add_filter( 'pre_get_posts', 'scl_menu_screen_add_private_pages' );
--- readme.txt
+++ readme.txt
@@ -1,23 +1,31 @@
 # Inclusive Parents
-Description: A tiny plugin to allow draft, private, scheduled, and password-protected pages to be selected as parents, displayed in menus, and included in page lists.
+Description: Allow draft, private, scheduled, and password-protected pages to be selected as parents and added to menus.
 Author: sillybean
-Version: 1.0.1
+Version: 1.1
 Author URI: http://stephanieleary.com
 License: GPL2
 Requires at least: 3.2
-Tested up to: 4.6
-Stable tag: 1.0.1
+Tested up to: 4.6.1
+Stable tag: 1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 
 ## Description
 
-A tiny plugin to allow draft, private, scheduled, and password-protected pages to be used normally throughout the admin screens. They may be selected as parents in page dropdowns, included in page lists, added to menus, and included in public page queries.
+Allow draft, private, scheduled, and password-protected pages to be selected as parents in page dropdowns. Private and password-protected pages may also be added to menus and the Pages widget.
 
 
 ## Changelog
-### 1.0.1
+
+### 1.1
+* Limit menu screen statuses to public, private, and password.
+* Use labels instead of names for (status) suffixes.
+* Add (status) suffix on menu screen.
+* Remove filter on public queries.
+
+### 1.0.1 
 * Prefix all the things
+
 ### 1.0
 * Initial release
